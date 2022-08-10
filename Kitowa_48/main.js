88c4e0eb8cb02e22eda87be1e0a3d8e8c7c0ba0c
cogcamp <codeofgenius.camp@gmail.com>
2022-08-10 06:32:47 +0000

    Kitowa_48

--- Kitowa_48/main.js
+++ Kitowa_48/main.js
@@ -34,13 +34,14 @@ mainScene.create = function () {
     this.createPlayer();
     
     // UI作成
-    
+    this.createUI();
     
     // コイン作成
-    
+    this.createCoin();
     
     // 敵作成
-    
+    this.createEnemyGroup();
+
     
     // ファイヤーグループ作成
     
@@ -56,8 +57,32 @@ mainScene.update = function() {
     if(this.isGameOver) {
         return false;
     }
+
+   if (this.cursors.left.isDown){
+       //左カーソルをクリックしたとき
+       this.player.body.setVelocityX(-this.runSpeed);
+       this.player.anims.play('walk',true);
+       this.player.flipX=true;
+       this.player.direction='left';
+   }else if (this.cursors.right.isDown){
+       //右カーソルキーをクリックしたとき
+       this.player.body.setVelocityX(this.runSpeed);
+       this.player.anims.play('walk',true)
+       this.player.flipX=false;
+       this.player.direction='right';
+   }else{
+       //カーソルキーをはなすと停止する
+       this.player.body.setVelocityX(0);
+       this.player.anims.stop();
+   }
+       //上カーソルキーをクリックしたとき
+       if(this.cursors.up.isDown&&this.player.body.onFloor()){
+          this.player.body.setVelocityY(-this.jumpPower);
+       }
+       
+       
     
-    
+   
     
 };
 
@@ -123,17 +148,35 @@ mainScene.createPlayer = function() {
 
 mainScene.createUI = function() {
     // 画面右上にスコアを表示する
-    
-    
-    
+    this.scoreText=this.add.text(650,50,'score:'+
+    this.score,{
+        fontSize:'30px Open Sans',
+        fill:'#ff0000'
+    });
+    //文字は固定表示(カメラに合わせて移動しない)
+    this.scoreText.setScrollFactor(0);
 };
+    
+    
+    
+
 
 mainScene.createCoin = function() {
     // コイン画像の読み込み
-    
-    
-    
+    var coinTiles=this.map.addTilesetImage('coin');
+    //　コインレイヤー作成
+    this.coinLayer=this.map.createDynamicLayer('Coin',
+    coinTiles,0,0);
+    //プレイヤーとコインレイヤーの衝突判定
+    this.physics.add.overlap(this.player,this.coinLayer);
+       // コインレイヤーとの衝突処理
+        this.coinLayer.setTileIndexCallback(17,this.collectCoin,
+        this);
 };
+    
+    
+    
+
 
 mainScene.collectCoin = function(sprite, tile) {
     // プレイヤーがコインに衝突
@@ -160,6 +203,13 @@ mainScene.createEnemyGroup = function() {
 
 mainScene.createEnemy = function() {
     // 敵を作成
+    //敵をランダムにする
+    var enemyType= Phaser.Math.RND.pick(this.enemyData);
+    //敵のX座標をランダムにする
+    var enemyPositionX=Phaser.Math.RND.between(500,2000);
+    //敵の作成
+    var enemy=this.enemies.create(enemyPositionX,100,
+    )
     
     
     
